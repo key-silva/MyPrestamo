f50e5ef0037be9002f9cbd1b42c61baf143c4ad2
Emilio Antonio Gaitan Fuentes <emiliogaitan00658@gmail.com>
2019-04-02 16:41:56 -0600

    Initial commit

---
+++ app/src/main/java/com/example/myappprestamo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myappprestamo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Spinner spinner=findViewById(R.id.textsexo);
+//        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.sexo ,android.R.layout.simple );
+    }
+}
