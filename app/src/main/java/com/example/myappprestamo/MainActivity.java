bcaa674e17de77c6397f82c7e425a2c2223511e3
Emilio Antonio Gaitan Fuentes <emiliogaitan00658@gmail.com>
2019-04-04 14:09:50 -0600

    nueva cambios

--- app/src/main/java/com/example/myappprestamo/MainActivity.java
+++ app/src/main/java/com/example/myappprestamo/MainActivity.java
@@ -2,16 +2,32 @@ package com.example.myappprestamo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Spinner spinner=findViewById(R.id.textsexo);
-//        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.sexo ,android.R.layout.simple );
+
+        btn=(Button)findViewById(R.id.buttonsiguiente);
+        btn.setOnClickListener(MainActivity.this);
     }
+    public void onClick(View v){
+        if(v==btn){
+            EditText nombre=findViewById(R.id.editTextnombre);
+            String cadena1=nombre.getText().toString();
+            if(cadena1.equals("")){
+                Toast.makeText(MainActivity.this,"Ingresar Nombre", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
